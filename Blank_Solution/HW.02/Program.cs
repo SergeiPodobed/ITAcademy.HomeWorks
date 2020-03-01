9b3be6aa61dd9c4e080bf3634b724267ade320ec
Компик Наташи) < )@DESKTOP-KEF0OEP>
2020-03-01 21:09:12 +0300

    Foto is ready

--- Blank_Solution/HW.02/Program.cs
+++ Blank_Solution/HW.02/Program.cs
@@ -1,28 +1,88 @@
 ﻿using System;
+using System.IO;
 
 namespace HW._02
 {
     class Program
     {
+               
         static void Main(string[] args)
         {
 
-            //1. Чтение файла по следующему пути: C:\Temp\C#\ДЗ 2\image.txt
+            //1. Обращаемся к файлу по следующему пути: C:\Temp\C#\HW_2\image.txt
 
-            //2. Создание массива бинарных чисел.
+            System.IO.StreamReader textReader = new StreamReader(@"C:\Temp\C#\HW_2\image.txt", true);
 
-            //3. Cоздание массива ImageBytes, который будет хранить данные типа bite (после конвертации).
+            //2. Считываем текст из файла до конца
 
-            //4. Считывание всего текста из файла.
+            string textReaderResult = textReader.ReadToEnd();
 
-            //5. Конвертирование бинарных кодов в байты фотографии.
+           //3. Разделяем на символы прочитанный текст.
 
-            //6. Освобождение ресурсов, использованных объектом TextReader
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+
+            //4. Создание массива бинарных чисел.
+
+
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];       
+                        
+
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+
+                
+            {
+
+                //5. Конвертирование бинарных кодов в байты фотографии.
+
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+
+                imageBytes[i] = binary;
+
+            }
+
+            textReader.Dispose();
+
+            
 
             //7. Вывод байтов фотографии в файл C:\Temp\C#\ДЗ 2\image.png
 
+            File.WriteAllBytes(@"C:\Temp\C#\HW_2\image.png", imageBytes);
 
-            Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+
+
+            
+
+
+
+            //3. Cоздание массива ImageBytes, который будет хранить данные типа bite (после конвертации).
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+            //6. Освобождение ресурсов, использованных объектом TextReader
+
+            
+
+
+            
         }
     }
 }
