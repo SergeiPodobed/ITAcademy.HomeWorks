90f276abe5c5f9b222359cb522343b24b7638fa2
SergeiPodobed <izotep19@yandex.ru>
2020-03-22 18:27:31 +0300

    task 6.3 is ready

---
+++ Task_6.3/Task_6.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_6._3
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            int[] mas1 = { 10, 20, 30, 40, 50, 60, 70, 80 };
+            int size = mas1.Length - 1;
+
+            Console.WriteLine("\nВывод массива по порядку:");
+
+            for (int i = 0; i <= size; i++)
+
+            {
+                Console.WriteLine(mas1[i]);
+
+            }
+
+            Console.WriteLine("\nВывод массива в обратном порядке:");
+
+            for (int i = size; i >= 0; i--)
+
+            {
+                Console.WriteLine(mas1[i]);
+
+            }
+
+
+
+        }
+        }
+    }
+
