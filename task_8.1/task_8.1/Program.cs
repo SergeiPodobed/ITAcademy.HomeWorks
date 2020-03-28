23af607e122633261cba4bcd6ef343fcf9d69270
SergeiPodobed <izotep19@yandex.ru>
2020-03-28 21:27:49 +0300

    task 8.1 is ready

---
+++ task_8.1/task_8.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task_8._1
+{
+    class Program
+    {
+        private static readonly string song;
+
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Введите с помощью клавиатуры стихотворение в одну строку" + "," + '\n' + "используя ; в качестве разделителя" + '\n');
+
+            string text = Console.ReadLine();
+            text = text.Replace("o", "a");
+            string[] arr_text = text.Split(new char[] { ';' });
+            
+                foreach (string s in arr_text)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
+        
+    
