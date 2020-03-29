bd830f0460ce6b85f59189867ef8b0e57207bb74
SergeiPodobed <izotep19@yandex.ru>
2020-03-29 20:17:02 +0300

    Заместил длинные слова на короткие

--- hw_8.2/hw_8.2/Program.cs
+++ hw_8.2/hw_8.2/Program.cs
@@ -10,22 +10,36 @@ namespace hw_8._2
             string text = "Каждый охотник желает знать где сидит фазан";
             Console.WriteLine($"Исходный текст \n{ text }");
             string[] words = text.Split(new char[] { ' ' });
+            string[] words2 = text.Split(new char[] { ' ' });
             string max = String.Empty;
-           
+            string min = String.Empty;
+
             for (int i = 0; i < words.Length; i++)
             {
-                string longword = words[i];
+               string longword = words[i];
 
-                if (longword.Length > max.Length)
-                {
+               if (longword.Length > max.Length)
+               {
                     max = longword;
-                    text = text.Replace(max, string.Empty);
-                }
+               }
+                text = text.Replace(max, " ");
             }
+
             Console.WriteLine();
             Console.WriteLine($"Текст без длинного слова \n{text}");
 
+            for (int i = 0; i < words2.Length; i++)
+            {
+                string shortword = words2[i];
 
+                if (shortword.Length < min.Length)
+                {
+                    min = shortword;
+                    text = text.Replace(max, min);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(($"Текст с заменой длинного слова на короткое \n{text.Replace(max, min)}"));
         }
     }
 }
