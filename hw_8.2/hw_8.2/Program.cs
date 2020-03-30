c7e9105642432a41105235507c3a6f457391f656
Sergei Podobed <61542934+SergeiPodobed@users.noreply.github.com>
2020-03-30 19:46:54 +0300

    Сортировка слов по убыванию (черновик)

--- hw_8.2/hw_8.2/Program.cs
+++ hw_8.2/hw_8.2/Program.cs
@@ -7,18 +7,21 @@ namespace hw_8._2
     {
         static void Main(string[] args)
         {
-            string text = "Каждый охотник желает знать где сидит фазан";
+            string text = "Каждый охотник желает знать где сидит фазан?!";
             Console.WriteLine($"Исходный текст \n{ text }");
             string[] words = text.Split(new char[] { ' ' });
             string[] words2 = text.Split(new char[] { ' ' });
+            string[] words3 = text.Split(new char[] { ' ' });
             string text_1 = text;
             string text_2 = text;
             string text_3 = text;
             string text_4 = text;
             string temp = "temp";
             string text_5 = text;
+            string text_6 = text;
             int a = 0;
-            int b = 0;
+            var b = 0;
+            int d = 0;
 
             string max = String.Empty;
             string min = "000000000000";
@@ -26,8 +29,7 @@ namespace hw_8._2
             for (int i = 0; i < words.Length; i++)
             {
                string longword = words[i];
-               string shortword = words2[i];
-
+               
                 if (longword.Length > max.Length)
                {
                     max = longword;
@@ -62,13 +64,34 @@ namespace hw_8._2
                 if ((text_5[i] >= 'а' && text_5[i] <= 'я') || (text_5[i] >= 'А' || text_5[i] <= 'Я'))
                     a++;
             }
-            
-           
+
+            foreach (var c in text_5) if (c == ',' || c == ';' || c == '!' || c == '?') ++b;
             
             Console.WriteLine();
             Console.WriteLine($"Количество букв равно {a}");
+            Console.WriteLine();
+            Console.WriteLine("Кол-во знаков препинания : {0}", b);
 
+            for (int i = 0; i < words3.Length; i++)
+            {
+                string sortword = words3[i];
+
+                if (sortword.Length > max.Length)
+                {
+                    max = sortword;
+                    d++;
+                    words3[i] = words3[d-i-1];
+                }   
+
+                foreach(int i in words3)
+                {
+                    Console.WriteLine(words3[i]);
+                }
             }
+
+
+
+        }
     }
 }
 
