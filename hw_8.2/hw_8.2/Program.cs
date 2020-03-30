7b8984a8c9d5db67c183328396b8314377c31241
Sergei Podobed <61542934+SergeiPodobed@users.noreply.github.com>
2020-03-30 16:43:43 +0300

    task_8.2 (replase min and max)

--- hw_8.2/hw_8.2/Program.cs
+++ hw_8.2/hw_8.2/Program.cs
@@ -11,35 +11,48 @@ namespace hw_8._2
             Console.WriteLine($"Исходный текст \n{ text }");
             string[] words = text.Split(new char[] { ' ' });
             string[] words2 = text.Split(new char[] { ' ' });
+            string text_1 = text;
+            string text_2 = text;
+            string text_3 = text;
+            string text_4 = text;
+            string temp = "temp";
+
             string max = String.Empty;
-            string min = String.Empty;
+            string min = "000000000000";
 
             for (int i = 0; i < words.Length; i++)
             {
                string longword = words[i];
+               string shortword = words2[i];
 
-               if (longword.Length > max.Length)
+                if (longword.Length > max.Length)
                {
                     max = longword;
                }
-                text = text.Replace(max, " ");
-            }
-
+                text_1 = text.Replace(max, " ");
+               }
             Console.WriteLine();
-            Console.WriteLine($"Текст без длинного слова \n{text}");
+            Console.WriteLine($"Длинное слово \n{max}");
+            Console.WriteLine();
+            Console.WriteLine($"Текст без длинного слова \n{text_1}");
 
+            
             for (int i = 0; i < words2.Length; i++)
             {
                 string shortword = words2[i];
-
+          
                 if (shortword.Length < min.Length)
                 {
                     min = shortword;
-                    text = text.Replace(max, min);
                 }
+                text_2 = text.Replace(max, temp);
+                text_3 = text_2.Replace(min, max);
+                text_4 = text_3.Replace(temp, min);
             }
             Console.WriteLine();
-            Console.WriteLine(($"Текст с заменой длинного слова на короткое \n{text.Replace(max, min)}"));
+            Console.WriteLine($"Короткое слово \n{min}");
+            Console.WriteLine();
+            Console.WriteLine(($"Текст с заменой длинного слова на короткое \n{text_4}"));
         }
     }
 }
