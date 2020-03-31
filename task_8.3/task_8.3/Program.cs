8a3e13ea689e8fdd480e911739a8000f4121ab13
Sergei Podobed <61542934+SergeiPodobed@users.noreply.github.com>
2020-03-31 18:20:17 +0300

    task_3 draft

---
+++ task_8.3/task_8.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace task_8._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("$ Исходные данные: 1a!2.3!!..4.!.? 6 7!.. ?");
+
+            StringBuilder sb1 = new StringBuilder("1a!2.3!!..4.!.? 6 7!.. ?");
+            sb1[] punkts = 
+
+
+            foreach (var c in sb1) if 
+            {
+
+            }
+
+
+           
+
+
+
+
+        }
+    }
+}
