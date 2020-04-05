cb8b1d1d53d5356deda6a5e1d2bfcc41ca896a13
SergeiPodobed <izotep19@yandex.ru>
2020-04-05 23:37:26 +0300

    task 1 in process

---
+++ AssemblyOne/AssemblyOne/Motorcycle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Schema;
+
+namespace AssemblyOne
+{
+    class Motorcycle
+    {
+        public const ushort MaxSpeed = 300;
+        public string vinNumber = "2C77799-7865-DSR-986";
+        public int odometr = 12_000;
+
+        public Motorcycle()
+        {
+        }
+
+        public void StartEngine()
+        {
+        }
+
+
+
+
+    }
+}
