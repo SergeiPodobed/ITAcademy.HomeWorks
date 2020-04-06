326f2034bda886173335704212a33d05a7a4d4f9
SergeiPodobed <izotep19@yandex.ru>
2020-04-06 17:50:55 +0300

    task 9.1 continue

--- AssemblyOne/AssemblyOne/Motorcycle.cs
+++ AssemblyOne/AssemblyOne/Motorcycle.cs
@@ -7,8 +7,10 @@ namespace AssemblyOne
 {
     class Motorcycle
     {
-        public const ushort MaxSpeed = 300;
-        public string vinNumber = "2C77799-7865-DSR-986";
+        
+        public const string vinNumber = "2C77799-7865-DSR-986";
+        public int movetime = 300;
+        public int power = 105;
         public int odometr = 12_000;
 
         public Motorcycle()
@@ -19,7 +21,19 @@ namespace AssemblyOne
         {
         }
 
+    class SportMotoCycle : Motorcycle
+    {
+            static void Main()
+                {
+                var middlespeed = new midlespeed();
+                var middlepower = new middlepower();
+                
+                middlespeed.odometr = 220;
+                middlepower.power = 300;
+                
+                }
 
+    }
 
 
     }
