586ab1483819af7b96ce4e24a0feb1b4c8f935f6
SergeiPodobed <izotep19@yandex.ru>
2020-04-12 19:46:15 +0300

    task 9_1 optimisation

--- AssemblyTwo/AssemblyTwo/Program.cs
+++ AssemblyTwo/AssemblyTwo/Program.cs
@@ -6,10 +6,16 @@ namespace AssemblyTwo
     {
         static void Main(string[] args)
         {
+            Motorcycle motorcycle3 = new Motorcycle();
 
-            motorcycle. ;
-            
-            Console.WriteLine("Hello World!");
+            motorcycle3.maxSpeed = 200;                 // по умолчанию private, не может быть вызвана в дрг классе
+            motorcycle3.vinNumber = "fff-444-555-ooo";  // private, не может быть вызвана в дрг классе
+            motorcycle3.odometr = 12_000;               // internal, доступен в люб месте тек проекта
+            motorcycle3.name = "BMW";                   // protected private, доступен в тек и произв классе
+            motorcycle3.age = 3;                        // protected internal, доступен в люб месте тек проекта и из наследн
+            motorcycle3.type = "bike";                  // public доступен в любом месте программы
+
+            Console.ReadKey();
         }
     }
 }
