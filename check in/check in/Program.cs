b7b1b285d900cfb8cea5cd565e0c041a59968dac
SergeiPodobed <izotep19@yandex.ru>
2020-04-12 20:07:14 +0300

    check in started

---
+++ check in/check in/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace check_in
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
