7f9cdd9f558f63fd2bba1477ab5aef021a1ec2f5
SergeiPodobed <izotep19@yandex.ru>
2020-04-18 01:00:50 +0300

    continue with hw 9 task 2

--- check in/check in/Program.cs
+++ check in/check in/Program.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 
+
 namespace check_in
 {
     class Program
     {
+        const string Bortnumber = "LH-44556";
+        const string Destination = "Larnaka";
+        const int Bortcapacity = 175;
         static void Main(string[] args)
         {
-           // константы: номер рейса, направление, максимальная вместимость самолета
-           // invitation, input: name, pasport; output: confirm, number in the queue, current date and time     
-           // вводимые данные пишем в массив размерностью - вместимость самолета 
-           // вопрос о багаже: (а) если ручная кладь, то присваевается место (2х мерный массив) и напвление на паспортный контроль
-           // (б) если багаж сдается, производится контроль веса, доплата за перевес, присваевается номер багажа и в пункт (а)
-           // на паспортном контроле запрашивается номер паспорта и проверяется его соответствие введенному номеру вначале  
-           // если все хорошо, выводится информация: фио, добро пожаловать на борт, след по маршр в г. Ларнака, место___  
+            Console.WriteLine($"Dear Madame/Sir pleas input your first then last name");
+            string Name = Console.ReadLine();
+            Console.WriteLine($"Pleas input your pasport number");
+            string Pasport = Console.ReadLine();
+            Random rnd = new Random();
+            int turn = rnd.Next(1, 175);
+            Console.WriteLine($"Your turn is {turn}, check in started at {DateTime.Now}");
+            Console.WriteLine($"If you have hand luggage push H");
+            int[] rows = new int [5];
 
+            // константы: номер рейса, направление, максимальная вместимость самолета
+            // invitation, input: name, pasport; output: confirm, number in the queue, current date and time     
+            // вводимые данные пишем в массив размерностью - вместимость самолета 
+            // вопрос о багаже: (а) если ручная кладь, то присваевается место (2х мерный массив) и напвление на паспортный контроль
+            // (б) если багаж сдается, производится контроль веса, доплата за перевес, присваевается номер багажа и в пункт (а)
+            // на паспортном контроле запрашивается номер паспорта и проверяется его соответствие введенному номеру вначале  
+            // если все хорошо, выводится информация: фио, добро пожаловать на борт, след по маршр в г. Ларнака, место___  
 
-            Console.WriteLine("Hello World!");
         }
     }
+
+
+
 }
