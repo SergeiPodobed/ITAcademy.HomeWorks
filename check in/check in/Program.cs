8b959dc980f6cf1f2dd73e8cd22d48306d0b4e18
SergeiPodobed <izotep19@yandex.ru>
2020-04-19 20:12:35 +0300

    ready but without new classes (

--- check in/check in/Program.cs
+++ check in/check in/Program.cs
@@ -9,12 +9,11 @@ namespace check_in
         const string Destination = "Larnaka";
         const int Bortcapacity = 175;
         const int extrapay = 9;
+        const int weight = 23;
         static void Main(string[] args)
         {
             Console.WriteLine($"Dear Madame/Sir pleas input your name");
-            string Name = Console.ReadLine();
             Console.WriteLine($"Pleas input your's pasport number");
-            string Pasport = Console.ReadLine();
             Random rnd = new Random();
             int turn = rnd.Next(1, 35);
             Console.WriteLine($"Your turn is {turn}, check in started at {DateTime.Now}");
@@ -27,50 +26,52 @@ namespace check_in
                     Console.WriteLine($"To reserve a sit choose a row A, B, C, D or E");
                     string row1 = Console.ReadLine();
                     if (row1 == "A")
-                        Console.WriteLine($"Your sit is {"A"}{turn}");
-                        Console.WriteLine($"Pleas go to the passport control");
+                        Console.WriteLine($"Your sit is {"A"}{turn}, {Bortnumber}, destination {Destination}");
+                        Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                     if (row1 == "B")
-                        Console.WriteLine($"Your sit is {"B"}{turn}");
-                        Console.WriteLine($"Pleas go to the passport control");
+                        Console.WriteLine($"Your sit is {"B"}{turn}, {Bortnumber}, destination {Destination}");
+                        Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                     if (row1 == "C")
-                        Console.WriteLine($"Your sit is {"C"}{turn}");
-                        Console.WriteLine($"Pleas go to the passport control");
+                        Console.WriteLine($"Your sit is {"C"}{turn}, {Bortnumber}, destination {Destination}");
+                        Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                     if (row1 == "D")
-                        Console.WriteLine($"Your sit is {"D"}{turn}");
-                        Console.WriteLine($"Pleas go to the passport control");
+                        Console.WriteLine($"Your sit is {"D"}{turn}, {Bortnumber}, destination {Destination}");
+                        Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                     if (row1 == "E")
-                        Console.WriteLine($"Your sit is {"E"}{turn}");
-                        Console.WriteLine($"Pleas go to the passport control");
+                        Console.WriteLine($"Your sit is {"E"}{turn}, {Bortnumber}, destination {Destination}");
+                        Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                     break;
+                
                 case "C":
                     Console.WriteLine($"Pleas put yours luggage on weight");
                     Random rnd1 = new Random();
-                    int pay = (rnd1.Next(5, 40) - 23) * extrapay;
+                    int weight1 = (rnd1.Next(5, 40));
+                    int pay = (weight1 - weight) * extrapay;
+                    Console.WriteLine($"Weight of luggage is {weight1}");
                     if (pay > 0)
                     {
-                        Console.WriteLine($"Pleas pay {pay * 10} eur");
+                        Console.WriteLine($"Pleas pay {pay * extrapay} eur");
                         Console.WriteLine($"Pleas enter sum for payment");
                         int sum = Convert.ToInt32(Console.ReadLine());
-                        if (sum == pay * 10)
+                        if (sum == pay * extrapay)
                         {
                             Console.WriteLine($"Pleas reserve a sit");
                             Console.WriteLine($"To reserve a sit choose a row A, B, C, D or E");
                             string row2 = Console.ReadLine();
                             if (row2 == "A")
-                                Console.WriteLine($"Your sit is {"A"}{turn}");
-                                Console.WriteLine($"Pleas go to the passport control");
+                                Console.WriteLine($"Your sit is {"A"}{turn}, {Bortnumber}, destination {Destination}");
+                                Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                             if (row2 == "B")
-                                Console.WriteLine($"Your sit is {"B"}{turn}");
-                                Console.WriteLine($"Pleas go to the passport control");
+                                Console.WriteLine($"Your sit is {"B"}{turn}, {Bortnumber}, destination {Destination}");
+                                Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                             if (row2 == "C")
-                                Console.WriteLine($"Your sit is {"C"}{turn}");
-                                Console.WriteLine($"Pleas go to the passport control");
+                                Console.WriteLine($"Your sit is {"C"}{turn}, {Bortnumber}, destination {Destination}");
+                                Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                             if (row2 == "D")
-                                Console.WriteLine($"Your sit is {"D"}{turn}");
-                                Console.WriteLine($"Pleas go to the passport control");
+                                Console.WriteLine($"Your sit is {"D"}{turn}, {Bortnumber}, destination {Destination}");
                             if (row2 == "E")
-                                Console.WriteLine($"Your sit is {"E"}{turn}");
-                                Console.WriteLine($"Pleas go to the passport control");
+                                Console.WriteLine($"Your sit is {"E"}{turn}, {Bortnumber}, destination {Destination}");
+                                Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                         }
                         else
                         {
@@ -82,28 +83,26 @@ namespace check_in
                         Console.WriteLine($"To reserve a sit choose a row A, B, C, D or E");
                         string row2 = Console.ReadLine();
                         if (row2 == "A")
-                            Console.WriteLine($"Your sit is {"A"}{turn}");
-                            Console.WriteLine($"Pleas go to the passport control");
+                            Console.WriteLine($"Your sit is {"A"}{turn}, {Bortnumber}, destination {Destination}");
+                            Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                         if (row2 == "B")
-                            Console.WriteLine($"Your sit is {"B"}{turn}");
-                            Console.WriteLine($"Pleas go to the passport control");
+                            Console.WriteLine($"Your sit is {"B"}{turn}, {Bortnumber}, destination {Destination}");
+                            Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                         if (row2 == "C")
-                            Console.WriteLine($"Your sit is {"C"}{turn}");
-                            Console.WriteLine($"Pleas go to the passport control");
+                            Console.WriteLine($"Your sit is {"C"}{turn}, {Bortnumber}, destination {Destination}");
+                            Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                         if (row2 == "D")
-                            Console.WriteLine($"Your sit is {"D"}{turn}");
-                            Console.WriteLine($"Pleas go to the passport control");
+                            Console.WriteLine($"Your sit is {"D"}{turn}, {Bortnumber}, destination {Destination}");
+                            Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                         if (row2 == "E")
-                            Console.WriteLine($"Your sit is {"E"}{turn}");
-                            Console.WriteLine($"Pleas go to the passport control");
+                            Console.WriteLine($"Your sit is {"E"}{turn}, {Bortnumber}, destination {Destination}");
+                            Console.WriteLine($"You are welcome on plane {Bortnumber}, destination {Destination}, have a good flight");
                     }
                     break;
                 default:
                     Console.WriteLine($"Pleas do reservation one more time");
                     break;
             }
-
-
         }
     }
 }
@@ -113,7 +112,6 @@ namespace check_in
 // вводимые данные пишем в массив размерностью - вместимость самолета 
 // вопрос о багаже: (а) если ручная кладь, то присваевается место (2х мерный массив) и напвление на паспортный контроль
 // (б) если багаж сдается, производится контроль веса, доплата за перевес, присваевается номер багажа и в пункт (а)
-// на паспортном контроле запрашивается номер паспорта и проверяется его соответствие введенному номеру вначале  
 // если все хорошо, выводится информация: фио, добро пожаловать на борт, след по маршр в г. Ларнака, место___  
 
 
